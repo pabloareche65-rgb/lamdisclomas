54beb0411492495bb081fe12ab56837abade586c
pabloareche65-rgb <pabloareche65@gmail.com>
2026-04-21 17:35:03 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        nombre: "Epson X123",
+        descripcion: "Lámpara completa",
+        precio: "$120.000",
+        imagen: "img/productos/epson.jpg",
+        categoria: "lamparas"
+    },
+    {
+        nombre: "BenQ MS524",
+        descripcion: "Lámpara completa",
+        precio: "$115.000",
+        imagen: "img/productos/benq.jpg",
+        categoria: "lamparas"
+    },
+    {
+        nombre: "ViewSonic PJD5155",
+        descripcion: "Lámpara completa",
+        precio: "$110.000",
+        imagen: "img/productos/viewsonic.jpg",
+        categoria: "lamparas"
+    },
+    {
+        nombre: "Bulbo genérico",
+        descripcion: "Alta intensidad",
+        precio: "$60.000",
+        imagen: "img/productos/bulbo1.jpg",
+        categoria: "bulbos"
+    }
+];
